d939b573b78cc96a9acd5844fa371d472e85f53c
TioNovriadiPutra <tionvriadi@gmail.com>
2024-07-11 09:37:48 +0700

    feat: add logger

--- config/logger.ts
+++ config/logger.ts
@@ -11,7 +11,7 @@ const loggerConfig = defineConfig({
    */
   loggers: {
     app: {
-      enabled: false,
+      enabled: true,
       name: env.get('APP_NAME'),
       level: env.get('LOG_LEVEL'),
       transport: {
